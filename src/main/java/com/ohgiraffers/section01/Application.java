8571a500848e591c7a2604aec337f0081f19b446
gony <goeun990@naver.com>
2024-03-15 14:48:17 +0900

    정고은

---
+++ src/main/java/com/ohgiraffers/section01/Application.java
@@ -0,0 +1,19 @@
+package com.ohgiraffers.section01;
+
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        TestService testService = new TestService();
+
+        do{
+
+        }
+
+    }
+
+}
---
+++ src/main/java/com/ohgiraffers/section01/TestService.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.section01;
+
+public class TestService {
+
+    private TestMapper mapper;
+
+
+
+}
